8c961126ea24581bdc65b4a9ca06bbf60f7d651f
wei song <48117135+penguinGOD@users.noreply.github.com>
2020-03-31 16:17:43 +0800

    code

---
+++ STM32 programmes/F103 C8T6/HARDWARE/Com_Lib/com_execute.c
@@ -0,0 +1,10 @@
+#include "com_prop.h"
+void ProcessData(COM_Frame* RecFrame)
+{
+	switch(RecFrame->Prop) 
+	{ 
+			
+					default :
+									break;
+	}
+}
---
+++ STM32 programmes/F103 ZET6/HARDWARE/LED.h
@@ -0,0 +1,5 @@
+#include "sys.h"
+#define LED0 PBout(5)// PB5
+#define LED1 PEout(5)// PE5	
+
+void LED_Init(void);
